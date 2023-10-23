6fbeb502f75ed1a2ace2f602e7f301f3c9f908a0
Gary McCormack <garymccormack@outlook.com>
2023-10-23 18:07:05 +0100

    Finished

---
+++ lib/dice_roller.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentRoll = 1;
+
+  void rollDice() {
+    setState(() {
+      currentRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentRoll.png',
+          width: 200,
+        ),
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(top: 40),
+                foregroundColor: Colors.white,
+                textStyle:
+                    const TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            child: const Text('Roll Dice'))
+      ],
+    );
+  }
+}
